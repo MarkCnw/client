565b40a69e3649a2089057a872e3698e7dc2623c
MarkCnw <chinnawong554@gmail.com>
2024-12-25 13:05:55 +0700

    week8

---
+++ src/app/member/member.component.ts
@@ -0,0 +1,8 @@
+import { Component, inject } from '@angular/core'
+import { environment } from '../../environments/environment'
+import { HttpClient } from '@angular/common/http'
+
+export class MemberComponent {
+
+}
+
